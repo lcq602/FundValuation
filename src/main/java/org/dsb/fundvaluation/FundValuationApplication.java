fbe4ec47dec6520ae3a5663aff00dcd899712dd5
lcq <2492065355@qq.com>
2026-05-25 09:27:05 +0800

    初始化

---
+++ src/main/java/org/dsb/fundvaluation/FundValuationApplication.java
@@ -0,0 +1,13 @@
+package org.dsb.fundvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundValuationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundValuationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dsb/fundvaluation/FundValuationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dsb.fundvaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundValuationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
